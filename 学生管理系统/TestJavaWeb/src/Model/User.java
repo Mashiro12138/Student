df8541ca99ee2ef8f9ce35236c55f385d634199c
Mashiro12138 <70941670+Mashiro12138@users.noreply.github.com>
2021-12-28 13:18:48 +0800

    Add files via upload

---
+++ 学生管理系统/TestJavaWeb/src/Model/User.java
@@ -0,0 +1,31 @@
+package Model;
+
+public class User {
+	private String username;	//’À∫≈
+	private String password;	//√‹¬Î
+	private String identity;	//…Ì∑›
+	
+	public void setUsername(String username){
+		this.username = username;
+	}
+	
+	public void setPassword(String password){
+		this.password = password;
+	}
+	
+	public void setIdentity(String identity){
+		this.identity = identity;
+	}
+	
+	public String getUsername(){
+		return username;
+	}
+	
+	public String getPassword(){
+		return password;
+	}
+	
+	public String getIdentity(){
+		return identity;
+	}
+}
